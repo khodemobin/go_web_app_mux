f989710e40de0faa0b27dd451202168daf447489
mobin <mobinyazdanparast9@gmail.com>
2021-04-11 18:02:02 +0430

    add chi router; add csrf middleware

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,25 +9,38 @@ import (
 	"net/http"
 )
 
+const port = "8080"
+
 func main() {
 	var app config.AppConfig
+	app.UseCache = false
 
-	// register app templates
+	registerTemplates(&app)
+	registerRepositories(&app)
+	registerRoutes(&app)
+}
+
+func registerTemplates(app *config.AppConfig) {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("can not create template cache", err)
 	}
 	app.TemplateCache = tc
-	app.UseCache = false
-	render.NewTemplates(&app)
+	render.NewTemplates(app)
+}
 
-	// register handler repositories
-	repo := handlers.NewRepo(&app)
+func registerRepositories(app *config.AppConfig) {
+	repo := handlers.NewRepo(app)
 	handlers.NewHandlers(repo)
+}
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
+func registerRoutes(app *config.AppConfig) {
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: routes(app),
+	}
 
-	fmt.Println("Listen to port 8000")
-	_ = http.ListenAndServe(":8000", nil)
+	fmt.Println("Listen to port ", port)
+	err := srv.ListenAndServe()
+	log.Fatalln(err)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,5 +1,22 @@
 package main
 
-//func routes(app *config.AppConfig) http.Handler {
-//
-//}
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/khodemobin/go_web_app_mux/pkg/config"
+	"github.com/khodemobin/go_web_app_mux/pkg/handlers"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
